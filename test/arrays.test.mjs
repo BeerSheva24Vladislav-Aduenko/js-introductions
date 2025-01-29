f9fca3ab67e8fa06c47b50408b0a274ba3535d31
VladislavAduenko <adyenko.job@gmail.com>
2025-01-29 11:21:13 +0200

    array destructuring

--- test/arrays.test.mjs
+++ test/arrays.test.mjs
@@ -1,45 +1,17 @@
 import { describe, it, expect } from "vitest";
-describe("inserting new elements in array", () => {
-  const inseredNumbers = [-10, -20];
-  it("inserting at begining of array", () => {
-    const ar = [1, 2];
-    const expected = [-10, -20, 1, 2];
-    ar.unshift(...inseredNumbers);
-    expect(ar).toEqual(expected);
-  });
-  it("adding new numbers at end of array", () => {
-    const ar = [1, 2];
-    const expected = [1, 2, -10, -20];
-    ar.push(...inseredNumbers);
-    expect(ar).toEqual(expected);
-  });
-  it("inserting new number at middle of array", () => {
-    const ar = [1, 2];
-    const expected = [1, -10, -20, 2];
-    ar.splice(1, 0, ...inseredNumbers);
-    expect(ar).toEqual(expected);
-  });
-});
+const array = [10, 1000, -10, 30, 60];
 
-describe("sorting array", () => {
-  const array = [10, 1000, -10, 30, 60];
-  const objects = [{ x: 4 }, { x: 5 }];
-  it("numbers native sorting soting", () => {
-    const expected = [-10, 10, 30, 60, 1000];
-    expect(array.toSorted((a, b) => a - b)).toEqual(expected);
+describe("destructuring array", () => {
+  it("assigning two first numbers of array to different variables", () => {
+    const [a, b] = array;
+    expect(a).toBe(array[0]);
+    expect(b).toBe(array[1]);
   });
-  it("includes", () => {
-    expect(array.includes({ x: 4 })).toBeFalsy();
-    expect(array.includes(1000)).toBeTruthy();
-    expect(array.includes(200)).toBeFalsy();
-  });
-
-  it("testing for array of objects", () => {
-    expect(objects.find((obj) => obj.x === 4)).toBeTruthy();
-    expect(objects.find((obj) => obj.x === 1)).toBeFalsy();
-  });
-
-  it("testing for all element match acondition", () => {
-    expect(array.every((num) => num % 2 === 0)).toBeTruthy();
+  it("swap of values by using destructuring", () => {
+    let a = 10;
+    let b = 20;
+    [b, a] = [a, b];
+    expect(a).toBe(20);
+    expect(b).toBe(10);
   });
 });
